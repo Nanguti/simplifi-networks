369f014d102de2ce69e7047f2151bb4642f42a28
Nanguti <g.nanguti@gmail.com>
2023-08-28 08:47:05 +0300

    sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,15 +1,16 @@
-'use client'
-import Link from "next/link";
+"use client";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import axiosClient from "@/utils/axios";
+
 const Sidebar = () => {
-    const router = useRouter()
-  const [brands, setBrands] = useState([])
+  const router = useRouter();
+  const [brands, setBrands] = useState([]);
+  const [brandCategories, setBrandCategories] = useState([]);
+
   useEffect(() => {
     fetchMenuItems("/menu-items");
   }, []);
-
   const fetchMenuItems = async (url) => {
     try {
       const response = await axiosClient.get(url);
@@ -18,56 +19,71 @@ const Sidebar = () => {
       console.error("Error fetching data:", error);
     }
   };
-
-  const handleProductsByBrand = (slug)=>{
-    router.push(`/brand/${slug}`);
-  }
-    return (
-        <>
-            <div className="uren-sidebar-catagories_area">
-                <div className="category-module uren-sidebar_categories">
-                    <div className="category-module_heading">
-                        <h5>Categories</h5>
-                    </div>
-                    <div className="module-body">
-                        <ul className="module-list_item">
-                            <li>
-                                <Link href="">Consoles </Link>
-                                <Link href="indoor-access-points-wifi-5.html">
-                                    WiFi{" "}
-                                </Link>
-                                <Link href="#">
-                                    Routing &amp; Switching{" "}
-                                </Link>
-                                <Link href="protect.html">Protect </Link>
-                                <Link href="#">UISP </Link>
-                                <Link href="#">LTU </Link>
-                                <Link href="#">Accessories </Link>
-                                <Link href="#">Routers </Link>
-                                <Link href="#">Switches </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="uren-sidebar_categories">
-                    <div className="uren-categories_title">
-                        <h5>Top Manufacturers</h5>
-                    </div>
-                    <ul className="sidebar-checkbox_list">
-                        {brands.map((brand)=>
-                        <li>
-                            <a className="cursor-pointer" onClick={()=> handleProductsByBrand(brand.slug) } >
-                                {brand.title} <span></span>
-                            </a>
-                        </li>
-                        )}
-                        
-                        
-                    </ul>
-                </div>
-            </div>
-        </>
+  const handleProductsByBrand = async (slug) => {
+    const storedCategories = JSON.parse(
+      localStorage.getItem("brandSubCategories")
     );
+
+    if (storedCategories && storedCategories[slug]) {
+      setBrandCategories(storedCategories[slug]);
+    } else {
+      await handleBrandCategories(slug);
+    }
+    localStorage.setItem("brand_slug", slug);
+    router.push(`/brand/${slug}`);
+  };
+
+  const handleBrandCategories = async (slug) => {
+    try {
+      const response = await axiosClient.post(`/category/sub-categories`, {
+        slug,
+      });
+      const category = response.data.category;
+      setBrandCategories(category.sub_categories);
+
+      const storedCategories =
+        JSON.parse(localStorage.getItem("brandSubCategories")) || {};
+      storedCategories[slug] = category.sub_categories;
+      localStorage.setItem(
+        "brandSubCategories",
+        JSON.stringify(storedCategories)
+      );
+    } catch (error) {
+      console.error(
+        "An error occurred. Could not fetch sub categories!",
+        error
+      );
+    }
+  };
+  return (
+    <>
+      <div className="uren-sidebar-catagories_area">
+        <div className="uren-sidebar_categories">
+          <div className="uren-categories_title">
+            <h5>Top Manufacturers</h5>
+          </div>
+          <ul className="sidebar-checkbox_list">
+            {brands.map((brand) => (
+              <li key={brand.id}>
+                <a
+                  className={`cursor-pointer ${
+                    localStorage.getItem("brand_slug") === brand.slug
+                      ? "active"
+                      : ""
+                  }`}
+                  onClick={() => {
+                    handleProductsByBrand(brand.slug);
+                  }}
+                >
+                  <span>{brand.title} </span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Sidebar;
