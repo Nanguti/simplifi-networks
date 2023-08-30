7650a17e0a3c60dd6f351e3bf94a2c7bacc2bb18
Nanguti <g.nanguti@gmail.com>
2023-08-30 18:46:13 +0300

    login and registration

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,5 +1,153 @@
-const SignUp = () => {
-  return <div>SignUp</div>;
+"use client";
+import { useState } from "react";
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    country: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      email: formData.email,
+      name: formData.firstName,
+      lastName: formData.lastName,
+      country: formData.country,
+      password: formData.password,
+      password_confirmation: formData.password_confirmation,
+    };
+    try {
+      await axiosClient.post("/register", data);
+      toast.success("You have successfully registered with Simplifi Networks!");
+    } catch (errors) {
+      toast.error(`Subscription failed. ${errors.response.data.errors.email}`);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">Register an Account</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="firstName" className="block mb-1 font-medium">
+              Email *
+            </label>
+            <input
+              type="text"
+              name="email"
+              className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
+              placeholder="Enter Email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="firstName" className="block mb-1 font-medium">
+              First Name *
+            </label>
+            <input
+              type="text"
+              name="firstName"
+              className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
+              placeholder="Enter your first name"
+              value={formData.firstName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="lastName" className="block mb-1 font-medium">
+              Last Name *
+            </label>
+            <input
+              type="text"
+              name="lastName"
+              className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
+              placeholder="Enter your last name"
+              value={formData.lastName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="lastName" className="block mb-1 font-medium">
+              Password *
+            </label>
+            <input
+              type="text"
+              name="password"
+              className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
+              placeholder="Enter your last name"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="lastName" className="block mb-1 font-medium">
+              Confirm Password *
+            </label>
+            <input
+              type="text"
+              name="password_confirmation"
+              className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
+              placeholder="Enter your last name"
+              value={formData.password_confirmation}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block mb-2 font-medium">Select Country</label>
+            <div>
+              <label className="flex items-center">
+                <input
+                  type="radio"
+                  value="Kenya"
+                  name="country"
+                  checked={true}
+                  onChange={handleInputChange}
+                  className="mr-2"
+                />
+                Kenya
+              </label>
+              <label className="flex items-center">
+                <input
+                  type="radio"
+                  value="Uganda"
+                  name="country"
+                  onChange={handleInputChange}
+                  className="mr-2"
+                />
+                Uganda
+              </label>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-700"
+          >
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default SignUp;
+export default RegistrationForm;
