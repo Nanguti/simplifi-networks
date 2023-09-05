7c8b6623b0561fd9bcc9aad7ac256f4987386c8e
Nanguti <g.nanguti@gmail.com>
2023-09-05 16:49:40 +0300

    revert

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -38,20 +38,23 @@ const Contact = () => {
                 </iframe>
               </div>
             </div>
-            <div style="width: 100%">
-              <iframe
-                width="100%"
-                height="600"
-                frameborder="0"
-                scrolling="no"
-                marginheight="0"
-                marginwidth="0"
-                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=uganda+(13%20C/A%20Bazarabusa%20Rd%20(Luthuli%20Drive),%20Bugolobi)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-              >
-                <a href="https://www.maps.ie/population/">
-                  Calculate population in area
-                </a>
-              </iframe>
+            <div className="col-lg-6 col-md-12 order-2 order-lg-1">
+              <h3 className="contact-page-title">Kampala, Uganda</h3>
+              <div style={{ width: "100%" }}>
+                <iframe
+                  width="100%"
+                  height={400}
+                  frameBorder={0}
+                  scrolling="no"
+                  marginHeight={0}
+                  marginWidth={0}
+                  src="https://maps.google.com/maps?width=100%25&height=400&hl=en&q=13%20C/A%20Bazarabusa%20Rd%20(Luthuli%20Drive),%20Bugolobi+(Simplifi%20Networks)&t=&z=14&ie=UTF8&iwloc=B&output=embed"
+                >
+                  &lt;a
+                  href="https://www.maps.ie/distance-area-calculator.html"&gt;area
+                  maps&lt;/a&gt;
+                </iframe>
+              </div>
             </div>
           </div>
         </div>
