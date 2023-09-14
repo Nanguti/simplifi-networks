ce12ae8e9c3ece617e460ecb089b987b500ae76c
Nanguti <g.nanguti@gmail.com>
2023-09-14 12:13:09 +0300

    Newsletter page.

--- app/newletters/page.jsx
+++ app/newletters/page.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 
-function TechTalks() {
+function Newsletter() {
   return (
     <div className="p-8">
-      <h2 className="text-2xl font-bold mb-4">Newletters</h2>
+      <h2 className="text-2xl font-bold mb-2">
+        <span className="relative inline-block">
+          Newsletter
+          <hr></hr>
+        </span>
+      </h2>
       <div className="flex flex-wrap -mx-4">
         <div className="w-1/2 px-4 mb-4">
           <div className="mt-2">
-            <p className="text-lg font-semibold">Video Title 1</p>
-            <p className="text-gray-500">Published by Channel Name</p>
+            <p className="text-gray-500">
+              {" "}
+              <a href="/assets/pdf/Simplifi_Newsletter_Oct_2021.pdf">
+                Simplifi Newsletter October 2021
+              </a>{" "}
+            </p>
           </div>
         </div>
 
-        {/* Add more pairs of video elements as needed */}
+        {/* Add more content as needed */}
       </div>
     </div>
   );
 }
 
-export default TechTalks;
+export default Newsletter;
