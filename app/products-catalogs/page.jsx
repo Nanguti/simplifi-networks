9a2b1333ab156e206b22a30ca0151a571000a911
Nanguti <g.nanguti@gmail.com>
2023-09-14 11:08:52 +0300

    Product catelog page.

--- app/products-catalogs/page.jsx
+++ app/products-catalogs/page.jsx
@@ -2,24 +2,40 @@ import React from "react";
 
 function ProductsCatalogs() {
   return (
-    <div className="p-8">
-      <h2 className="text-2xl font-bold mb-4">Products Catelogue</h2>
-      <div className="flex flex-wrap -mx-4">
-        <div className="w-1/2 px-4 mb-4">
-          <div className="mt-2">
-            <p className="text-lg font-semibold">Video Title 1</p>
-            <p className="text-gray-500">Published by Channel Name</p>
-          </div>
-        </div>
-
-        <div className="w-1/2 px-4 mb-4">
-          <div className="mt-2">
-            <p className="text-lg font-semibold">Video Title 1</p>
-            <p className="text-gray-500">Published by Channel Name</p>
-          </div>
-        </div>
-
-        {/* Add more pairs of video elements as needed */}
+    <div className="container">
+      <div className="overflow-x-auto mb-10 mt-8 ">
+        <div className="gradient-title">Product Catalog</div>
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead>
+            <tr>
+              <th className="px-8 py-4 bg-gray-100 text-left text-lg leading-6 font-medium text-gray-600 uppercase tracking-wider">
+                Number
+              </th>
+              <th className="px-8 py-4 bg-gray-100 text-left text-lg leading-6 font-medium text-gray-600 uppercase tracking-wider">
+                Name/Title
+              </th>
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+            <tr>
+              <td className="px-8 py-6 whitespace-no-wrap">1</td>
+              <td className="px-8 py-6 whitespace-no-wrap">
+                <a href="/assets/pdf/ReyeeWirelessProductOverview.pdf">
+                  Reyee Wireless Product Overview
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td className="px-8 py-6 whitespace-no-wrap">2</td>
+              <td className="px-8 py-6 whitespace-no-wrap">
+                <a href="/assets/pdf/ReyeeProductCatalog2023.pdf">
+                  Reyee Product Catalog 2023
+                </a>{" "}
+              </td>
+            </tr>
+            {/* Add more rows as needed */}
+          </tbody>
+        </table>
       </div>
     </div>
   );
