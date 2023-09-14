e3d9cf3f6e62a0b2330cab1513eb0c79fd92d42f
Nanguti <g.nanguti@gmail.com>
2023-09-14 11:23:30 +0300

    News and articles page.

--- app/news-articles/page.jsx
+++ app/news-articles/page.jsx
@@ -2,38 +2,40 @@ import React from "react";
 
 function NewsArticle() {
   return (
-    <div className="p-8">
-      <h2 className="text-2xl font-bold mb-4">News And Articles</h2>
+    <div className="p-14">
+      <h2 className="text-2xl font-bold mb-4">News & Articles</h2>
       <div className="flex flex-wrap -mx-4">
         <div className="w-1/2 px-4 mb-4">
           <div className="relative overflow-hidden rounded-lg">
-            {/* Replace the iframe source with your YouTube video URL */}
+            {/* Replace the iframe source with the YouTube embed URL */}
             <iframe
-              src="https://www.youtube.com/watch?time_continue=10&v=zuXgaVFqcn0&embeds_referring_euri=https%3A%2F%2Fsimplifinetworks.com%2F&source_ve_path=MjM4NTE&feature=emb_title"
+              src="https://www.youtube.com/embed/zuXgaVFqcn0"
               frameBorder="0"
               allowFullScreen
               className="w-full h-56 md:h-64 lg:h-72"
             ></iframe>
           </div>
           <div className="mt-2">
-            <p className="text-lg font-semibold">Video Title 1</p>
-            <p className="text-gray-500">Published by Channel Name</p>
+            <p className="text-lg font-semibold">News</p>
           </div>
         </div>
 
         <div className="w-1/2 px-4 mb-4">
           <div className="relative overflow-hidden rounded-lg">
-            {/* Replace the iframe source with your YouTube video URL */}
+            {/* Replace the iframe source with the YouTube embed URL */}
             <iframe
-              src="https://www.youtube.com/watch?v=6zb6CtcM8Hs&embeds_referring_euri=https%3A%2F%2Fsimplifinetworks.com%2F&source_ve_path=MjM4NTE&feature=emb_title"
+              src="https://www.youtube.com/embed/6zb6CtcM8Hs"
               frameBorder="0"
               allowFullScreen
               className="w-full h-56 md:h-64 lg:h-72"
             ></iframe>
           </div>
+
           <div className="mt-2">
-            <p className="text-lg font-semibold">Video Title 2</p>
-            <p className="text-gray-500">Published by Channel Name</p>
+            <p className="text-lg font-semibold">Articles</p>
+            <p className="text-gray-500">
+              Wifi 6 will continue to grow at huge levels - By Ken Stober
+            </p>
           </div>
         </div>
 
